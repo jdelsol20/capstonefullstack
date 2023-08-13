a2a9b3154683cea30b00f1d3c7e422cde467042f
Jose <jdelsol20@gmail.com>
2023-08-13 03:30:11 -0700

    Updated line 11 for react router dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
+import  Home  from './components/Home'
+import  Cart  from './components/Cart'
+
 
 function App() {
   return (
-    <div className="App">
-     <Header />
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/cart" element={<Cart/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
