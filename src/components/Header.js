55420eb345cf306743ee0ed14cde30a990c44b5c
Jose <jdelsol20@gmail.com>
2023-08-13 00:26:04 -0700

    Started updating header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,13 @@
+import { Container, Navbar } from "react-bootstrap"
 
+// See more infor about navbar here https://react-bootstrap-v4.netlify.app/components/navbar/
 const Header = () => {
   return (
-    <div>Header</div>
+    <Navbar>
+        <Container>
+            <Navbar.Brand></Navbar.Brand>
+        </Container>
+    </Navbar>
   )
 }
 
